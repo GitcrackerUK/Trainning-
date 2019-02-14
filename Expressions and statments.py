f9f8cea3908498d996033d4b5ad39dee7f44ccc9
gitcrackerUK <pfsiwek@gmail.com>
2019-02-14 20:13:20 +0100

    Added few data types and examples.

---
+++ Expressions and statments.py
@@ -0,0 +1,59 @@
+#Values and data tapes
+
+type(123)
+#<class 'int'>
+
+type(True)
+#<class 'bool'>
+
+type(False)
+#<class 'bool'>
+
+type(123.34)
+#<class 'float'>
+
+type(234.234)
+#<class 'float'>
+
+type("2343566")
+#<class 'str'>
+
+type("Wello World")
+#<class 'str'>
+
+type('one Million fifty thousand ')
+#<class 'str'>
+
+type('He looked at her and said "solid work is crucial for advancing in every aspect of life"')
+#<class 'str'>
+
+
+
+#type("tell"02734855)
+  #File "<input>", line 1
+    #type("tell"02734855)
+                     #^
+#SyntaxError: invalid token
+
+#Varibles
+
+#To define variables we can use long names and numbers but name
+#has too start from a letter. We can't use special signs
+#for variables names.
+
+message="that's very interesting point sir"
+n=123
+pi=3.145
+twelve="why,what,where?"
+
+type(n)
+#<class 'int'>
+
+type(pi)
+#<class 'float'>
+
+type(twelve)
+#<class 'str'>
+
+type(message)
+#<class 'str'>
