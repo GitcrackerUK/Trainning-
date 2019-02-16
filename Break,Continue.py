7c0d000ddd0be84e2ea7885a5d1a1caa698e2001
gitcrackerUK <pfsiwek@gmail.com>
2019-02-16 14:53:42 +0100

    non

--- Break,Continue.py
+++ Break,Continue.py
@@ -1,3 +1,31 @@
+for i in range (0,20):
+    print(i)
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# 19
+
+for i in range (0,20):
+    if i == 11:
+        print(i)
+# 11
+
 
 for i in range (0,20):
     if i == 11:
@@ -15,15 +43,6 @@ for i in range (0,20):
 # 8
 # 9
 # 10
-
-
-
-for i in range (0,20):
-    if i == 11:
-        print(i)
-
-# 11
-
 
 
 for i in range (0,20):
