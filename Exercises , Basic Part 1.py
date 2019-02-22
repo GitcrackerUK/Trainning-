58a1f6f552eb1c1e9a47b20dc76be4d3c2976f50
gitcrackerUK <pfsiwek@gmail.com>
2019-02-22 19:47:08 +0100

    Exercise 6

--- Exercises , Basic Part 1.py
+++ Exercises , Basic Part 1.py
@@ -56,9 +56,17 @@ Twinkle, twinkle, little star,
 # Write a Python program which accepts the user's first and last name
 # and print them in reverse order with a space between them.
 
-fname=input("Enter your first name, please:")
-lname=input("Enter your last  name, please:")
-print("Hello",lname,fname)
+# fname=input("Enter your first name, please:")
+# lname=input("Enter your last  name, please:")
+# print("Hello",lname,fname)
 
 #6
+#Write a Python program which accepts a sequence of comma-separated
+# numbers from user and generate a list and a tuple with those numbers.
+
+base=input("Enter list of numbers separated by commas:")
+list=base.split(",")
+tuple=tuple(list)
+print("List:",list)
+print("Tuple:",tuple)
 
