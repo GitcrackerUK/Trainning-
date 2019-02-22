d96cd4fec0a256da580297850ad146d1e02288ef
gitcrackerUK <pfsiwek@gmail.com>
2019-02-22 14:58:10 +0100

    loop session 2
    for
    
    while
    
    if,elif, else

--- loops.py
+++ loops.py
@@ -1,104 +1,181 @@
-#for loop
-from typing import List
-
-x: List[str]=[]
-for i in range(1500,2701): # creation of loop in range between 1500 and 2701
-    if (i % 7==0) and (i % 5 ==0): #condition that if number in range from above meet cryteria (is divisiable by 7 and 5) value should be passed further down.
-        x.append(str(i))  #value filtered by condition above is stored in varible x  as a string.
-print(','.join(x))  #values from varible x is separeted by comma and printed
-print(len(x))      #this method is printing quantity of vules in variable x
-
-
-test=[]
-for i in range(0,150):
-    if i % 6 == 0:
-        test.append(str(i))
-print(test)
-print(','.join(test))
-
-
-
-three=[]
-for i in range(200,240):
-    if i % 3 ==0:
-        three.append(str(i))
-print('.'.join(three))
-
-
-
-
-session=[]
-for i in range (10,100):
-    if (i % 7 ==0) and (i % 9 == 0):
-        session.append(str(i))
-print(session)
-
-
-totu=[]
-tot=[]
-for i in range (0,100):
-    if (i % 3 ==0) and ( i % 5 == 0):
-        tot.append(str(i))
-totu=(','.join(tot))
-print(totu)
-
-
-
-
-
-
-
-
-
-
-
-
-
-x=[]
-for i in range(1500,2701):
-    if (i % 7 == 0) and (i % 5 == 0):
-        x.append(str(i))
-
-print('\n'.join(x))
-print(len(x))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# #for loop
+# from typing import List
+#
+# x: List[str]=[]
+# for i in range(1500,2701): # creation of loop in range between 1500 and 2701
+#     if (i % 7==0) and (i % 5 ==0): #condition that if number in range from above meet cryteria (is divisiable by 7 and 5) value should be passed further down.
+#         x.append(str(i))  #value filtered by condition above is stored in varible x  as a string.
+# print(','.join(x))  #values from varible x is separeted by comma and printed
+# print(len(x))      #this method is printing quantity of vules in variable x
+#
+#
+# test=[]
+# for i in range(0,150):
+#     if i % 6 == 0:
+#         test.append(str(i))
+# print(test)
+# print(','.join(test))
+#
+#
+#
+# three=[]
+# for i in range(200,240):
+#     if i % 3 ==0:
+#         three.append(str(i))
+# print('.'.join(three))
+#
+#
+#
+#
+# session=[]
+# for i in range (10,100):
+#     if (i % 7 ==0) and (i % 9 == 0):
+#         session.append(str(i))
+# print(session)
+#
+#
+# totu=[]
+# tot=[]
+# for i in range (0,100):
+#     if (i % 3 ==0) and ( i % 5 == 0):
+#         tot.append(str(i))
+# totu=(','.join(tot))
+# print(totu)
+#
+#
+#
+#
+
+
+
+
+
+
+
+# T=[]
+# for i in range(1500,2701):
+#     if (i % 7 == 0) and (i % 5 == 0):
+#         T.append(str(i))
+# print('\n'.join(T))
+# x=1500
+# y=2701
+# w=5
+#
+# def divisible(x,y,w):
+#     T=[]
+#     for i in range(x,y):
+#         if (i % w == 0):
+#             T.append(str(i))
+#     return ('\n'.join(T))
+#
+# print(divisible(x,y,w))
+
+
+
+# x=int(input("range from"))
+# y=int(input("range to"))
+# w=int(input("divisible  by"))
+# z=int(input("divisible  by"))
+#
+#
+# def divisible(x,y,w,z):
+#     T=[]
+#     for i in range(x,y):
+#         if (i % w == 0) and (i % z == 0):
+#             T.append(str(i))
+#     return T
+# print(divisible(x,y,w,z))
+
+
+
+# #while loop
+# z= 6
+# x= 0
+# while x < z:
+#     pass
+#
+# #for loop
+# for i in x:
+#     pass
+#
+#
+# #if elif else loop
+# z=0
+# if i == i:
+#     pass
+# elif i >= z:
+#     pass
+# else:
+#     pass
+#
+# # nested loop
+# i=0
+# x=0
+# if i == x:
+#     if i == x:
+#         if i == x:
+#             if i == x:
+#                 for i in x:
+#                     if i == x:
+#                         pass
+#
+#                     if i == x:
+#                         pass
+#
+#                     elif i == x:
+#                         pass
+#
+#                     else:
+#                         pass
+#
+#             elif i == x:
+#                 if i == x:
+#                     pass
+#                 if i == x:
+#                     pass
+#
+#                 elif i == x:
+#                     pass
+#
+#                 else:
+#                     pass
+#
+#             else:
+#                 pass
+#
+#         if i == x:
+#             pass
+#
+#         elif i == x:
+#             pass
+#
+#
+#         else:
+#             pass
+#
+#     if i == x:
+#         pass
+#
+#     elif i == x:
+#         pass
+#
+#     else :
+#         pass
+# if i != z:
+#     if i == x:
+#         pass
+#
+#     if i == x:
+#         pass
+#
+#     elif i == x:
+#         pass
+#
+#     else:
+#         pass
+# elif x== 1:
+#     pass
+# else:
+#     pass
 
 
