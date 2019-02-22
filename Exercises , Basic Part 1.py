9041afbd8105caa7d1b843fb28bcce2c6fade88d
gitcrackerUK <pfsiwek@gmail.com>
2019-02-22 16:30:24 +0100

    none

---
+++ Exercises , Basic Part 1.py
@@ -0,0 +1,41 @@
+1
+# Write a Python program to print the following string in a specific format
+
+#Sample string:
+"""Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"""
+
+#Output expected:
+"""Twinkle, twinkle, little star,
+	 How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	bHow I wonder what you are"""
+
+# To print String in new line, need be used \n to make extra horizontal tab need be used \n\t
+
+#print("Twinkle , twinkle, litle star,\n\t How I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+"""Twinkle, twinkle, little star,
+	 How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	bHow I wonder what you are"""
+
+2
+# Write a Python program to get the Python version you are using.
+
+# To check python version need be used sys.version command
+# import sys
+# print(sys.version)
+#
+# 3.6.7 (default, Oct 22 2018, 11:32:17)
+# [GCC 8.2.0]
+
+# 3 Write a Python program to display the current date and time.
+
+#To disply current date and time need be imported mpdule
+
+import datetime
+print(time.datetime)
