9578ad61f4368278cce502441e7bc0207ad9f48b
gitcrackerUK <pfsiwek@gmail.com>
2019-06-24 19:48:42 +0100

    exe 6-17

---
+++ exe/14.py
@@ -0,0 +1,5 @@
+from datetime import date
+data=date(2019,6,24)
+datb=date(2019,8,24)
+thisrt=datb-data
+print(thisrt.days)
---
+++ exe/15.py
@@ -0,0 +1,5 @@
+import math
+pi = 3.1415926535897931
+x=6
+v=4.0/3.0*pi*x**3
+print("The volume of the sphere is:", v)
